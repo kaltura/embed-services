855b3c1261448b52064643e8fcea3a6b81edd845
orenme <oren.melamed@kaltura.com>
2014-10-30 13:45:28 +0200

    Bump version v0.2.rc3

--- Configuration/defaultSettings.php
+++ Configuration/defaultSettings.php
@@ -1,6 +1,6 @@
 <?php
     // The version of the library:
-    $wgEmbedServicesVersion = '0.2.rc2';
+    $wgEmbedServicesVersion = '0.2.rc3';
 
     // Default debug mode
     $wgEnableScriptDebug = false;
