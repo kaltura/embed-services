b43bb02b6ef8bc94cfe6712438329d0ce9428fa5
Oren Me <oren.me@gmail.com>
2015-05-26 16:44:37 +0300

    Bump version v0.3.rc6

--- Configuration/DefaultSettings.php
+++ Configuration/DefaultSettings.php
@@ -1,6 +1,6 @@
 <?php
     // The version of the library:
-    $wgEmbedServicesVersion = '0.3.rc5';
+    $wgEmbedServicesVersion = '0.3.rc6';
 
     // Default debug mode
     $wgEnableScriptDebug = false;
