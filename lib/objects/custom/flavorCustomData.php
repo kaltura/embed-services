e273c851ad3d66f726c25858d741901be744ad10
Oren Me <oren.me@gmail.com>
2016-06-20 13:40:57 +0300

    Removed call for encryption service
    For on the fly smooth stream to dash on chrome the content id is needed
    to create the WV modular license request.
    The content id is taken from the actual protection header in the
    manifest so no need to query it from udrm.

--- lib/objects/custom/flavorCustomData.php
+++ lib/objects/custom/flavorCustomData.php
@@ -3,7 +3,6 @@
 class flavorCustomData {
 
 	var $data;
-	public $requireSerialization = true;
 	private $logger;
 
 	function __construct() {
@@ -49,19 +48,6 @@ class flavorCustomData {
                         "custom_data" => $custom_data,
                         "signature" => $signature
                     );
-
-                    if (isset($val["URL"]) && (strpos($val["URL"], 'ism') !== false))
-                    {
-                        $encryptionUrl = $wgKalturaUdrmEncryptionServer."?custom_data=".$custom_data."&signature=".$signature;
-                        $this->logger->info("UDRM encryption request: " . $encryptionUrl);
-                        $response = $this->getJson($encryptionUrl);
-                        $this->logger->debug("UDRM encryption response: " . json_encode($response));
-                        $contentId = "";
-                        if (isset($response) && is_array($response) && isset($response[0]) && isset($response[0]["key_id"])){
-                            $contentId = $response[0]["key_id"];
-                        }
-                        $flavorCustomData[ $val["FileID"] ]["contentId"] = $contentId;
-                    }
 
                     $this->logger->debug("Flavor UDRM data: " . json_encode($flavorCustomData[ $val["FileID"] ]));
 
